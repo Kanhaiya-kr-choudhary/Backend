1e8392a7ec4e113ece1bc603a71b723ab56052f9
Kanhaiya Kumar Choudhary <kumarkanhaiyachoudhary@gmail.com>
2025-12-13 23:47:37 +0530

    mistake fixs

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,7 +4,7 @@ const asyncHandler = (requestHandler) => async (req, res, next) => {
   } catch (error) {
     res.status(error.code || 500).json({
       sucess: false,
-      message: err.message,
+      message: error.message,
     });
   }
 };
