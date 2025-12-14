9a2819acc7c5219bef000ee8a65261a656bae15a
Kanhaiya Kumar Choudhary <kumarkanhaiyachoudhary@gmail.com>
2025-12-14 23:31:14 +0530

    done with models and jwt

---
+++ src/models/user.models.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      require: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      require: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+      require: true,
+    },
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      require: [true, "Password is required to login"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", UserSchema);
