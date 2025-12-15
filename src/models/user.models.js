3199edbea353cd58381a3f639b840483e6173d13
Kanhaiya Kumar Choudhary <kumarkanhaiyachoudhary@gmail.com>
2025-12-15 23:26:25 +0530

    using bcrypt for password privay

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -49,4 +49,10 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("Password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
 export const User = mongoose.model("User", UserSchema);
